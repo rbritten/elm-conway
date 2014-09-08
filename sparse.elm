0b8db0f5415e5db27fe490057be5a6c13a7725df
Jonathan Chow <me@echo.sh>
2014-09-09 00:19:26 +1200

    Optimises algorithm a bit.

--- sparse.elm
+++ sparse.elm
@@ -2,34 +2,33 @@ import Set
 
 -- STATES
 
-glider  = [(1,1),(2,1),(3,1),(3,0),(2,-1)]
-acorn   = [(1,0),(3,1),(0,2),(1,2),(4,2),(5,2),(6,2)]
-diehard = [(6,0),(0,1),(1,1),(1,2),(5,2),(6,2),(7,2)]
-gosper  = [] -- todo
+glider  = Set.fromList [(1,1),(2,1),(3,1),(3,0),(2,-1)]
+acorn   = Set.fromList [(1,0),(3,1),(0,2),(1,2),(4,2),(5,2),(6,2)]
+diehard = Set.fromList [(6,0),(0,1),(1,1),(1,2),(5,2),(6,2),(7,2)]
+gosper  = Set.fromList [] -- todo
 
 -- LOGIC
 
-main = lift render (foldp (\_ -> step) diehard (fps 30))
+main = lift render (foldp (\_ -> step) diehard (fps 60))
 
-
-step state = let set = Set.fromList state
-             in filter (\x ->
+step state = Set.filter (\x ->
                let cnt  = countLive x state
-                   live = inSet set x
+                   live = x `Set.member` state
                    bWhenLive = cnt == 3 || cnt == 4
                    bWhenDead = cnt == 3
                in (live && bWhenLive) || (not live && bWhenDead)) (cellsToCheck state)
 
-cellsToCheck = Set.toList . Set.fromList . concatMap around
+cellsToCheck = Set.foldl (\x out -> foldl Set.insert out (around x)) Set.empty
 
-countLive (x,y) state = let cells = Set.fromList (around (x,y))
-                        in length (filter (inSet cells) state)
+countLive pos state = foldl (\x cnt ->
+                        if x `Set.member` state then cnt + 1 else cnt
+                      ) 0 (around pos)
 
-around (x,y) = map (\(dx,dy) -> (x+dx,y+dy)) (liftL2 (,) [-1..1] [-1..1])
-
-liftL2 f x y = concatMap (\y -> map (\x -> f x y) x) y
-
-inSet set x = Set.member x set
+around (x,y) = [
+                 (x-1,y-1),(x,y-1),(x+1,y-1),
+                 (x-1,y),  (x,y),  (x+1,y),
+                 (x-1,y+1),(x,y+1),(x+1,y+1)
+               ]
 
 -- GFX
 
@@ -38,8 +37,10 @@ cellSize   = 10
 width      = 600
 height     = 600
 
-render state = collage width height (map dot state)
+render state = Set.toList state |> map dot
+                                |> collage width height
 
-dot p = move (pos p) (filled red (circle circleSize))
+dot p = circle circleSize |> filled red
+                          |> move (pos p)
 
 pos (x,y) = (toFloat (x*cellSize),toFloat (y*cellSize))
